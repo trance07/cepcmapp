3b083c8aeaff569e7cb1a0642f793e97a848b895
marco navarrete <marcopolonavarrete@marcos-macbook-pro.local>
2018-07-20 21:42:47 -0500

    Initial Commit

---
+++ CepcmApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CepcmApp
+//
+//  Created by marco polo navarrete on 7/20/18.
+//  Copyright © 2018 marco polo navarrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
