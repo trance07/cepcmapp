54fbe408549a51580c7482b591bc49ca04a4e59c
JCC <carlos.castro@gnp.com.mx>
2018-10-09 10:29:44 -0500

    Se agrega utileria de Fonts

---
+++ Utils/Fonts.swift
@@ -0,0 +1,46 @@
+//
+//  Fonts.swift
+//  CepcmApp
+//
+//  Created by Juan Carlos Castro Martinez on 09/10/18.
+//  Copyright © 2018 marco polo navarrete. All rights reserved.
+//
+
+import Foundation
+class Fonts: NSObject {
+    
+    class func avenirLight(size: Float) -> UIFont {
+        return UIFont(name: "Avenir-Light", size: CGFloat(size))!
+    }
+    
+    class func avenirNextRegular(size: Float) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: CGFloat(size))!
+    }
+    
+    class func AvenirNextDemiBold(size: Float) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: CGFloat(size))!
+    }
+    
+    class func AvenirMedium(size: Float) -> UIFont {
+        return UIFont(name: "Avenir-Medium", size: CGFloat(size))!
+    }
+    
+    class func AvenirBook(size: Float) -> UIFont {
+        return UIFont(name: "Avenir-Book", size: CGFloat(size))!
+    }
+    
+    class func ArialMT(size: Float) -> UIFont {
+        return UIFont(name: "ArialMT", size: CGFloat(size))!
+    }
+    
+    class func AvenirMediumBold(size:Float) -> UIFont {
+        return UIFont(name: "Avenir-Bold", size: CGFloat(size))!
+    }
+    
+    class func AvenirNextMediumBold(size:Float) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: CGFloat(size))!
+    }
+    
+    
+    
+}
