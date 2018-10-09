ee7d5eae5bbba49b905ac2497f0ba9ce76210616
JCC <carlos.castro@gnp.com.mx>
2018-10-09 10:08:58 -0500

    Se agrega utileria de colores

--- CepcmApp/AppDelegate.swift
+++ CepcmApp/AppDelegate.swift
@@ -34,6 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+            
+        UINavigationBar.appearance().tintColor = Colors.brightOrange()
+    
         return true
     }
 
---
+++ Utils/Colors.swift
@@ -0,0 +1,71 @@
+//
+//  Colors.swift
+//  CepcmApp
+//
+//  Created by Juan Carlos Castro Martinez on 09/10/18.
+//  Copyright © 2018 marco polo navarrete. All rights reserved.
+//
+
+import UIKit
+
+class Colors: NSObject {
+    
+    class func backgroundGray() -> UIColor {
+        return UIColor(red: 249.0/255.0, green: 250.0/255.0, blue: 252.0/255.0, alpha: 1.0)
+    }
+    
+    class func darkColor() -> UIColor {
+        return UIColor(red: 23.0/255.0, green: 36.0/255.0, blue: 52.0/255.0, alpha: 1.0)
+    }
+    
+    class func tabGrayColor() -> UIColor {
+        return UIColor(red: 156.0/255.0, green: 174.0/255.0, blue: 190.0/255.0, alpha: 1.0)
+    }
+    
+    class func brightOrange() -> UIColor {
+        return UIColor(red: 255.0/255.0, green: 87.0/255.0, blue: 10.0/255.0, alpha: 1.0)
+    }
+    
+    class func mangoColor() -> UIColor {
+        return UIColor(red: 255.0/255.0, green: 132.0/255.0, blue: 41.0/255.0, alpha: 1.0)
+    }
+    
+    class func steelGrey() -> UIColor {
+        return UIColor(red: 123.0/255.0, green: 130.0/255.0, blue: 139.0/255.0, alpha: 1.0)
+    }
+    
+    class func grayTextColor() -> UIColor {
+        return UIColor(red: 121.0/255.0, green: 140.0/255.0, blue: 160.0/255.0, alpha: 1.0)
+    }
+    
+    class func vigenteGreenColor() -> UIColor {
+        return UIColor(red: 67.0/255.0, green: 196.0/255.0, blue: 92.0/255.0, alpha: 1.0)
+    }
+    
+    class func dotGreen() -> UIColor {
+        return UIColor(red: 67.0/255.0, green: 196.0/255.0, blue: 92.0/255.0, alpha: 1.0)
+    }
+    
+    class func dotRed() -> UIColor {
+        return UIColor(red: 255.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+    
+    class func separatorGray() -> UIColor {
+        return UIColor(red: 218.0/255.0, green: 228.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+    }
+    
+    class func duskBlue() -> UIColor {
+        return UIColor(red: 31.0/255.0, green: 63.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+    }
+    
+    class func circleBorder() -> UIColor {
+        return UIColor(red: 218.0/255.0, green: 228.0/255.0, blue: 238.0/255.0, alpha: 1.0)
+    }
+    
+    class func colorPhone() -> UIColor {
+        return UIColor(red: 24.0/255.0, green: 61.0/255.0, blue: 123.0/255.0, alpha: 1.0)
+    }
+    
+    
+    
+}
